c38c5112b072ae119a72d05f92f610c5085e77b9
unknown <rin.tran@outlook.com>
2016-06-16 14:32:01 +0700

    first commit

---
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 10;
+
+
+    Animator anim;
+    GameObject player;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    bool playerInRange;
+    float timer;
+
+
+    void Awake ()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player");
+        playerHealth = player.GetComponent <PlayerHealth> ();
+        enemyHealth = GetComponent<EnemyHealth>();
+        anim = GetComponent <Animator> ();
+    }
+
+
+    void OnTriggerEnter (Collider other)
+    {
+		// neu no la player thi nam trong pham vi tan cong
+        if(other.gameObject == player)
+        {
+            playerInRange = true;
+        }
+    }
+
+
+    void OnTriggerExit (Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            playerInRange = false;
+        }
+    }
+
+
+    void Update ()
+    {
+        timer += Time.deltaTime;
+		// quay lai chu ki ban vs nam trong pham vi tan cong
+        if(timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth > 0)
+        {
+            Attack ();
+        }
+
+        if(playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger ("PlayerDead");
+        }
+    }
+
+
+    void Attack ()
+    {
+		//  reset thoi gian sau moi lan ban
+        timer = 0f;
+
+        if(playerHealth.currentHealth > 0)
+        {
+            playerHealth.TakeDamage (attackDamage);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed;
+    
+
+	private Vector3 movement;
+	private Animator anim;
+	private Rigidbody rb;
+	private int floorMask;
+	private float camRayLength = 100.0f;
+
+
+	void Awake(){
+		floorMask = LayerMask.GetMask ("Floor");
+		anim = GetComponent<Animator> ();
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate(){
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		// Move the player around the scene.
+		Move (h, v);
+		
+		// Turn the player to face the mouse cursor.
+		Turning ();
+		
+		// Animate the player.
+		Animating (h, v);
+	}
+
+	void Move(float h, float v){
+		movement.Set (h, 0.0f, v);
+		movement = movement.normalized * speed * Time.deltaTime;
+		rb.MovePosition (transform.position + movement);
+	}
+
+	void Turning(){
+		// lay vi tri chuot
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit floorHit;
+		if (Physics.Raycast(camRay,out floorHit, camRayLength, floorMask)){
+			Vector3 playerToMouse = floorHit.point - transform.position;
+			playerToMouse.y = 0f;
+			Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+			rb.MoveRotation(newRotation);
+		}
+
+	}
+
+	void Animating (float h, float v)
+	{
+		// Create a boolean that is true if either of the input axes is non-zero.
+		bool walking = h != 0f || v != 0f;
+		
+		// Tell the animator whether or not the player is walking.
+		anim.SetBool ("IsWalking", walking);
+	}
+
+	void Start(){
+
+	}
+}
